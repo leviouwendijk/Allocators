bc39dd8cff5f73d7ec669d16fa4817b5c15553bc
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-15 00:25:01 +0100

    changes to defaultallocator

--- Sources/Allocators/allocator/default-allocator.swift
+++ Sources/Allocators/allocator/default-allocator.swift
@@ -1,7 +1,9 @@
 import Foundation
 
 #if ALLOC_DEBUG
+// compiles with .inspector.leaks(), .inspector.report() for memory leak inspection
 public typealias DefaultAllocator = DebugAllocator<GenericAllocator>
 #else
-public typealias DefaultAllocator = GenericAllocator
+// will currently compile the same call sites as DebugAllocator, but does not inspect:
+public typealias DefaultAllocator = GenericAllocator 
 #endif
